d2dc99e3a84a8139eb70beb3398220eb22afe498
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-05-01 12:11:37 +0530

    version 3 (connecting and creating books in DB))

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import _ from "lodash";
 import axios from "axios";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let books = [];
+mongoose.connect('mongodb://127.0.0.1:27017/bookDB');
 
-app.get("/", (req, res) => {
-  res.render("your-books.ejs", { books: books });
+const bookSchema = {
+  title: String,
+  review: String,
+  author: String
+}
+const Book = mongoose.model("Book", bookSchema)
+
+
+app.get("/", async (req, res) => {
+
+  try{
+ const findBooks = await Book.find({});
+ console.log(findBooks)
+ res.render("your-books.ejs", { books: findBooks });
+  }catch(error){
+    console.error("Error fetching items:", error);
+    res.send("Internal Server Error");
+
+  }
+ 
 });
 
 app.get("/add-book", (req, res) => {
@@ -19,12 +38,14 @@ app.get("/add-book", (req, res) => {
 });
 
 app.post("/add-book", (req, res) => {
-  const newBook = {
+  const newBook = new Book({
     title: req.body.title,
-    description: req.body.description,
+    review: req.body.review,
     author: req.body.author,
-  };
-  books.push(newBook);
+  });
+    
+  
+  newBook.save();
   res.redirect("/");
 });
 app.get("/search-book", (req,res) =>{
@@ -47,15 +68,20 @@ app.post("/search-book",async (req,res) =>{
   
 });
 
-app.get("/books/:title", (req, res) => {
-  const params = _.lowerCase(req.params.title);
-  books.forEach((book) => {
-    const bookTitle = _.lowerCase(book.title);
-    if (params === bookTitle) {
-      res.render("book.ejs", { book: book });
-    }
-  });
+app.get("/books/:id", async (req, res) => {
+  try {
+    const requestBookId = req.params.id;
+    const foundBook = await Book.findById({_id: requestBookId})
+     res.render("book.ejs", {book:foundBook})
+
+  
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    res.send("Internal Server Error");
+  }
 });
+
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
